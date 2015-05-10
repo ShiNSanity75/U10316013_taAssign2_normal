983c3257569290f4d4c3a3f4e6c96c2143cb8294
ShiNSanity75 <jj06280705@gmail.com>
2015-05-10 23:19:05 +0800

    Circle的基本視窗設定

---
+++ Circle.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+/**Main Method*/
+public class CirCle extends JFrame{
+
+	public static void main(String[] args) {
+		CirCle frame = new CirCle(); //Create a frame
+	    frame.setTitle("U10316013_GeoCalculate"); //set the title of frame
+	    frame.setSize(500, 350); //Set the size of frame
+	    frame.setLocationRelativeTo(null); // Center the frame
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true); //Display the frame
+	}
+}
