0b1d553bf2e7c05637762706f8ffa1f48df78e99
ShiNSanity75 <jj06280705@gmail.com>
2015-05-12 21:40:39 +0800

    計算面積的視窗的基本設定

---
+++ Area.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+/**Main Method*/
+public class Area extends JFrame{
+
+	
+	
+	public static void main(String[] args) {
+		Area frame = new Area(); //Create a frame
+	    frame.setTitle("U10316013_GeoCalculate"); //set the title of frame
+	    frame.setSize(500, 350); //Set the size of frame
+	    frame.setLocationRelativeTo(null); // Center the frame
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true); //Display the frame
+	}
+}
