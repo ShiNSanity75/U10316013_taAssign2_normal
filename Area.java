c03b030d632a8e89076f1ff7f0c6ce3aded7f15f
ShiNSanity75 <jj06280705@gmail.com>
2015-05-14 21:58:03 +0800

    先把其他形狀的東西放上去，排版再改

--- Area.java
+++ Area.java
@@ -7,54 +7,122 @@ import java.awt.event.*;
 /**Main Method*/
 public class Area extends JFrame{
 	
-	//Create a button
-	JButton count = new JButton("Count");
-	JButton clear = new JButton("Clear");
+	//Create buttons
+	JButton countC = new JButton("Count");
+	JButton clearC = new JButton("Clear");
+	JButton countS = new JButton("Count");
+	JButton clearS = new JButton("Clear");
+	JButton countT = new JButton("Count");
+	JButton clearT = new JButton("Clear");
 	
-	//Create TextField
+	//Create TextFields
 	JTextField radius = new JTextField(3);
 	JTextField areaC = new JTextField(3);
 	JTextField perimeterC = new JTextField(3);
+	JTextField sideS = new JTextField(3);
+	JTextField areaS = new JTextField(3);
+	JTextField perimeterS = new JTextField(3);
+	JTextField sideT = new JTextField(3);
+	JTextField areaT = new JTextField(3);
+	JTextField perimeterT = new JTextField(3);
 	
 	//Radius that user enter
 	double radiusUser;
+	//side that user enter
+	double sideSUser;
+	double sideTUser;
+	
 
 	
 	public Area() {
 		
-		//Create a panel to put everthing of circle
+		//Create a panel to put everthing of circle, square and triangle
 		JPanel allCircle = new JPanel();
+		JPanel allSquare = new JPanel();
+		JPanel allTriangle = new JPanel();
 		
-		//Create a panel to display the title of Circle
+		
+		//Create a panel to display the title of Circle, square and triangle
 		JPanel titleCircle = new JPanel();
 		JLabel circle = new JLabel("Circle");
 		titleCircle.add(circle);
 		
-		//Create a panel to let user enter radius
+		JPanel titleSquare = new JPanel();
+		JLabel square = new JLabel("Square");
+		titleSquare.add(square);
+		
+		JPanel titleTriangle = new JPanel();
+		JLabel triangle = new JLabel("Regular Triangle");
+		titleTriangle.add(triangle);
+		
+		
+		//Create a panel to let user enter radius or side
 		JPanel userCircle = new JPanel();
 		userCircle.setLayout(new GridLayout(1, 4, 10, 10));
 		userCircle.add(new JLabel("Radious"));
 		userCircle.add(radius); 
-		userCircle.add(count); //add button
-		userCircle.add(clear); //add button
+		userCircle.add(countC); //add button
+		userCircle.add(clearC); //add button
 		setLayout(new FlowLayout());
 		
+		JPanel userSquare = new JPanel();
+		userSquare.setLayout(new GridLayout(1, 4, 10, 10));
+		userSquare.add(new JLabel("Side"));
+		userSquare.add(sideS); 
+		userSquare.add(countS); //add button
+		userSquare.add(clearS); //add button
+		setLayout(new FlowLayout());
+		
+		JPanel userTriangle = new JPanel();
+		userTriangle.setLayout(new GridLayout(1, 4, 10, 10));
+		userTriangle.add(new JLabel("Side"));
+		userTriangle.add(sideT); 
+		userTriangle.add(countT); //add button
+		userTriangle.add(clearT); //add button
+		setLayout(new FlowLayout());
+		
+		
 		//Create a panel to display the area and the perimeter
-		JPanel answer = new JPanel();
-		answer.setLayout(new GridLayout(1, 2, 10, 10));
+		JPanel answerC = new JPanel();
+		answerC.setLayout(new GridLayout(1, 2, 10, 10));
 		userCircle.add(new JLabel("Area"));
 		userCircle.add(areaC);
 		userCircle.add(new JLabel("Perimeter"));
 		userCircle.add(perimeterC);
 		
+		JPanel answerS = new JPanel();
+		answerS.setLayout(new GridLayout(1, 2, 10, 10));
+		userSquare.add(new JLabel("Area"));
+		userSquare.add(areaS);
+		userSquare.add(new JLabel("Perimeter"));
+		userSquare.add(perimeterS);
+		
+		JPanel answerT = new JPanel();
+		answerT.setLayout(new GridLayout(1, 2, 10, 10));
+		userTriangle.add(new JLabel("Area"));
+		userTriangle.add(areaT);
+		userTriangle.add(new JLabel("Perimeter"));
+		userTriangle.add(perimeterT);
+		
+		
 		//put panel together
 		allCircle.add(titleCircle);
 		allCircle.add(userCircle);
-		allCircle.add(answer);
+		allCircle.add(answerC);
+		
+		allSquare.add(titleSquare);
+		allSquare.add(userSquare);
+		allSquare.add(answerS);
+		
+		allTriangle.add(titleTriangle);
+		allTriangle.add(userTriangle);
+		allTriangle.add(answerT);
 		
 		add(allCircle);
-		count.addActionListener(new ButtonCount());
-		clear.addActionListener(new ButtonClear());
+		add(allSquare);
+		add(allTriangle);
+		countC.addActionListener(new ButtonCount());
+		clearC.addActionListener(new ButtonClear());
 		
 	}
 	
@@ -63,7 +131,7 @@ public class Area extends JFrame{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			
-			if(e.getSource() == count) {
+			if(e.getSource() == countC) {
 				//get the radius that user enter
 				radiusUser = Double.parseDouble(radius.getText());
 			}
@@ -82,7 +150,7 @@ public class Area extends JFrame{
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			
-			if(e.getSource() == clear) {
+			if(e.getSource() == clearC) {
 				
 				//Clear the radius, area and perimeter
 				radius.setText(null);
